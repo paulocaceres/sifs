c435b15a51c9bf04bba1e3f33b0a669aad655a8b
juanedu <juanedu@gmail.com>
2014-03-24 23:39:45 +0000

    Cambio en el comentario

--- HolaManola/src/HolaManola.java
+++ HolaManola/src/HolaManola.java
@@ -1,8 +1,7 @@
-
 public class HolaManola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Comentario Modificado
 		System.out.println("Hola Manola!!");
 	}
 
