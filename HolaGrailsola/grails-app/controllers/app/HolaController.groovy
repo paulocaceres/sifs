354325ce2c09f2fdd0c648d62726292844b25d64
juanedu <juanedu@gmail.com>
2014-03-25 03:48:02 +0000

    Hola Mundo en Grails: por linea de comando y usando MVC sin model por ahora

---
+++ HolaGrailsola/grails-app/controllers/app/HolaController.groovy
@@ -0,0 +1,13 @@
+package app
+
+class HolaController {
+
+	// Ver el tutorial en http://grails.asia/grails-tutorial-for-beginners/
+    def index() {
+		
+	}
+	
+	def hi(){
+		// render (view: "index.gsp")
+	}
+}
