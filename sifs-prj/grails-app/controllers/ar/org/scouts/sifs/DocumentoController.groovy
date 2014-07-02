0f28bc040444cf575bc70f992ea4cd994d3a56de
prieto.emiliano <prieto.emiliano@gmail.com>
2014-07-02 04:52:55 +0000

    Se modifican paquetes ar.org.scouts.sifs.controller y ar.org.scouts.sifs.domain por ar.org.scouts.sifs

---
+++ sifs-prj/grails-app/controllers/ar/org/scouts/sifs/DocumentoController.groovy
@@ -0,0 +1,107 @@
+package ar.org.scouts.sifs
+
+
+
+import static org.springframework.http.HttpStatus.*
+import ar.org.scouts.sifs.Documento;
+import grails.transaction.Transactional
+import grails.plugins.springsecurity.Secured
+
+@Transactional(readOnly = true)
+@Secured(['IS_AUTHENTICATED_FULLY'])
+class DocumentoController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Documento.list(params), model:[documentoInstanceCount: Documento.count()]
+    }
+
+    def show(Documento documentoInstance) {
+        respond documentoInstance
+    }
+
+    def create() {
+        respond new Documento(params)
+    }
+
+    @Transactional
+    def save(Documento documentoInstance) {
+        if (documentoInstance == null) {
+            notFound()
+            return
+        }
+
+        if (documentoInstance.hasErrors()) {
+            respond documentoInstance.errors, view:'create'
+            return
+        }
+
+        documentoInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'documentoInstance.label', default: 'Documento'), documentoInstance.id])
+                redirect documentoInstance
+            }
+            '*' { respond documentoInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Documento documentoInstance) {
+        respond documentoInstance
+    }
+
+    @Transactional
+    def update(Documento documentoInstance) {
+        if (documentoInstance == null) {
+            notFound()
+            return
+        }
+
+        if (documentoInstance.hasErrors()) {
+            respond documentoInstance.errors, view:'edit'
+            return
+        }
+
+        documentoInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Documento.label', default: 'Documento'), documentoInstance.id])
+                redirect documentoInstance
+            }
+            '*'{ respond documentoInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Documento documentoInstance) {
+
+        if (documentoInstance == null) {
+            notFound()
+            return
+        }
+
+        documentoInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Documento.label', default: 'Documento'), documentoInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'documentoInstance.label', default: 'Documento'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ sifs-prj/grails-app/controllers/ar/org/scouts/sifs/HistorialController.groovy
@@ -0,0 +1,107 @@
+package ar.org.scouts.sifs
+
+
+
+import static org.springframework.http.HttpStatus.*
+import ar.org.scouts.sifs.Historial;
+import grails.transaction.Transactional
+import grails.plugins.springsecurity.Secured
+
+@Transactional(readOnly = true)
+@Secured(['IS_AUTHENTICATED_FULLY'])
+class HistorialController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Historial.list(params), model:[historialInstanceCount: Historial.count()]
+    }
+
+    def show(Historial historialInstance) {
+        respond historialInstance
+    }
+
+    def create() {
+        respond new Historial(params)
+    }
+
+    @Transactional
+    def save(Historial historialInstance) {
+        if (historialInstance == null) {
+            notFound()
+            return
+        }
+
+        if (historialInstance.hasErrors()) {
+            respond historialInstance.errors, view:'create'
+            return
+        }
+
+        historialInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'historialInstance.label', default: 'Historial'), historialInstance.id])
+                redirect historialInstance
+            }
+            '*' { respond historialInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Historial historialInstance) {
+        respond historialInstance
+    }
+
+    @Transactional
+    def update(Historial historialInstance) {
+        if (historialInstance == null) {
+            notFound()
+            return
+        }
+
+        if (historialInstance.hasErrors()) {
+            respond historialInstance.errors, view:'edit'
+            return
+        }
+
+        historialInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Historial.label', default: 'Historial'), historialInstance.id])
+                redirect historialInstance
+            }
+            '*'{ respond historialInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Historial historialInstance) {
+
+        if (historialInstance == null) {
+            notFound()
+            return
+        }
+
+        historialInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Historial.label', default: 'Historial'), historialInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'historialInstance.label', default: 'Historial'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
