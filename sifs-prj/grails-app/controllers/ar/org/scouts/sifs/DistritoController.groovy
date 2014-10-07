1501af096109426295872e6a798499640f642b56
prieto.emiliano <prieto.emiliano@gmail.com>
2014-10-07 10:46:58 +0000

    Tags de seguridad para nuevos controllers de Distrito y Grupo.

--- sifs-prj/grails-app/controllers/ar/org/scouts/sifs/DistritoController.groovy
+++ sifs-prj/grails-app/controllers/ar/org/scouts/sifs/DistritoController.groovy
@@ -1,7 +1,11 @@
 package ar.org.scouts.sifs
 
 import grails.converters.JSON
+import grails.plugins.springsecurity.Secured
+import grails.transaction.Transactional
 
+@Transactional(readOnly = true)
+@Secured(['IS_AUTHENTICATED_FULLY'])
 class DistritoController {
 
     def index() { }
--- sifs-prj/grails-app/controllers/ar/org/scouts/sifs/GrupoController.groovy
+++ sifs-prj/grails-app/controllers/ar/org/scouts/sifs/GrupoController.groovy
@@ -1,5 +1,10 @@
 package ar.org.scouts.sifs
 
+import grails.plugins.springsecurity.Secured
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+@Secured(['IS_AUTHENTICATED_FULLY'])
 class GrupoController {
 
     def index() { }
