a10599f5537a6a597a55ef8fe7927ab85529b7b8
prieto.emiliano <prieto.emiliano@gmail.com>
2015-03-17 12:01:53 +0000

    Se eliminan la lista de correlativas del curso ya que la misma es un atributo de la relacion PlanCurso, es decir un mismo curso puede tener diferentes correlativas segun el plan en el que este. Se relaciona many-to-many con Plan

--- sifs-prj/grails-app/domain/ar/org/scouts/sifs/Curso.groovy
+++ sifs-prj/grails-app/domain/ar/org/scouts/sifs/Curso.groovy
@@ -2,31 +2,30 @@ package ar.org.scouts.sifs
 
 
 class Curso {
-	
+
 	String nombre
 	String descripcion
 	Nivel nivel // [Nacional|Internacional]
-/*
-	static belongsTo = [
-		plan: Plan
-	]
-*/	
+
+	static belongsTo = Plan
+
 	static hasMany = [
-		correlativas: Curso,
+   		planes: Plan,
 		contenidos: Contenido,
 		dictados: Dictado
 	]
-	
-    static constraints = {
+
+	static constraints = {
 //		contenidos(nullable:true)
 //		plan(nullable:true)
 		nivel(nullable:true)
+		nombre(unique: true)
 	}
-	
+
 	String toString() {
 		"$nombre"
 	}
-	
+
 	// this additional setter is used in the multiselect list to convert the ids selected in the checkbox list to the corresponding domain objects
 	public void setCursosIds(List ids) {
 		this.correlativas = ids.collect { Curso.get(it) }
