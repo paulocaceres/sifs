0f9f459388b577d51305d0ca4fbf441ecc34a8ff
paulocaceres <sifs.paulo@gmail.com>
2015-08-27 16:29:38 -0300

    Se agrego ordenamiento por columnas en la lista de persona

--- sifs-prj/grails-app/controllers/ar/org/scouts/sifs/PersonaController.groovy
+++ sifs-prj/grails-app/controllers/ar/org/scouts/sifs/PersonaController.groovy
@@ -51,7 +51,8 @@ class PersonaController {
 	@Secured(['ROLE_SUPERVISOR','ROLE_ADMIN'])
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-		params.sort = "documentoNumero"
+		params.sort = params.sort ?: "documentoNumero"
+		params.order = params.order ?: "asc"
 		if (!springSecurityService.currentUser.hasRol(Rol.findByAuthority('ROLE_ADMIN'))) {
 			def lista = Persona.findAllBySupervisor(springSecurityService.currentUser, params)
 			respond lista, model:[personaInstanceCount: lista.size]
@@ -61,7 +62,8 @@ class PersonaController {
     }
 
 	def indexSupervised(Integer max) {
-		params.sort = "documentoNumero"
+		params.sort = params.sort ?: "documentoNumero"
+		params.order = params.order ?: "asc"
 		params.max = Math.min(max ?: 10, 100)
 		def lista = Persona.findAllBySupervisor(springSecurityService.currentUser, params)
 		redirect(action: 'index', params: [personaInstanceList: lista, personaInstanceCount: lista.size])
